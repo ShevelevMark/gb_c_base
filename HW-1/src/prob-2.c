90fa58a96a7e0af598ec111184e4ca9c00c15c5e
ShevelevMark <shevelev.mm@yandex.ru>
2024-05-29 19:12:33 +0300

    Вторая задача

---
+++ HW-1/src/prob-2.c
@@ -0,0 +1,13 @@
+/**
+ * Задание 2. Вывести на экран текст "лесенкой"
+ * Let’s
+ *   go
+ *     to walk
+ * **/
+
+#include <stdio.h>
+
+int main() {
+    printf("Let's\n\tgo\n\t\tto walk\n");
+    return 0;
+}
