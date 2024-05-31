223655eb98fa504a9c1f256bb94c67aaedf203cb
ShevelevMark <shevelev.mm@yandex.ru>
2024-05-31 17:32:30 +0300

    Первая задача

---
+++ HW-7/src/prob-1.c
@@ -0,0 +1,22 @@
+/**
+ * Ввести с клавиатуры массив из 5 элементов, найти среднее арифметическое всех элементов массива.
+ * **/
+
+#include <stdio.h>
+
+int main() {
+    int nums[5];
+    for (int *it = nums, *end = nums + 5; it != end; ++it)
+        if (1 != scanf("%d", it)) {
+            printf("Error");
+            return 1;
+        }
+    
+    float avg = 0.f;
+    for (int *it = nums, *end = nums + 5; it != end; ++it)
+        avg += (float)*it;
+    avg /= 5.f;
+    printf("%.3f\n", avg);
+
+    return 0; 
+}
